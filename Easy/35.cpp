c0c92b36a00fcd63678f804b1d2265d87e136a99
kimdoseong <spl8714@gmail.com>
2022-10-12 00:20:40 +0900

    35-binary search

--- Easy/35.cpp
+++ Easy/35.cpp
@@ -3,15 +3,44 @@
 //
 
 #include <vector>
+#include <iostream>
+#include <cmath>
 
 using namespace std;
 
 class Solution {
-
 public:
     int searchInsert(vector<int> &nums, int target) {
+        auto first = nums.begin();
+        auto last = nums.end();
 
+        while (true) {
+            auto range_length = distance(first, last);
+            auto mid = first + floor(range_length / 2);
+            auto mid_idx = floor(range_length / 2);
+
+            if (*mid == target) {
+                return static_cast<int>(mid - nums.begin());
+            } else if (*mid > target) {
+                advance(last, -mid_idx);
+            } else {
+                advance(first, mid_idx);
+            }
+
+            if (range_length == 1) {
+                if (*first > target) {
+                    return static_cast<int>(distance(nums.begin(), first));
+                } else {
+                    return static_cast<int>(distance(nums.begin(), last));
+                }
+            }
+        }
     }
 };
 
-int main() {}
+int main() {
+    vector<int> nums{1, 3, 5, 6};
+
+    Solution s;
+    cout << s.searchInsert(nums, 0) << endl;
+}
