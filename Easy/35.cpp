5c4b4ff02404a187d5e2c85f582b6f58d8b7fd00
kimdoseong <spl8714@gmail.com>
2022-10-11 12:14:39 +0900

    Init

---
+++ Easy/35.cpp
@@ -0,0 +1,17 @@
+//
+// Created by kimdoseong on 2022-10-11.
+//
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+
+public:
+    int searchInsert(vector<int> &nums, int target) {
+
+    }
+};
+
+int main() {}
